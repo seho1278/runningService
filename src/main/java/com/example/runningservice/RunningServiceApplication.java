c0c3a05b5957d03e0a41d14fd79c7b3961e8fc4f
Kang Yeong Woo <rkdduddn8812@naver.com>
2024-08-07 21:43:08 +0900

    Initial commit

---
+++ src/main/java/com/example/runningservice/RunningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.runningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningServiceApplication.class, args);
+    }
+
+}
