dcbaf0974874f220d7aac6d51ae6ff798b5b96fb
yeon <p-yo00@naver.com>
2024-08-12 03:49:14 +0900

    test : 크루 생성 서비스 테스트 추가
    테스트 내용:
    - 크루 이미지 유무에 따른 생성
    - 사용자 없을 시 Exception 발생

---
+++ src/test/java/com/example/runningservice/service/CrewServiceTest.java
@@ -0,0 +1,117 @@
+package com.example.runningservice.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.example.runningservice.dto.crew.CrewRequestDto.Create;
+import com.example.runningservice.dto.crew.CrewResponseDto.CrewData;
+import com.example.runningservice.entity.CrewEntity;
+import com.example.runningservice.entity.MemberEntity;
+import com.example.runningservice.exception.CustomException;
+import com.example.runningservice.repository.CrewMemberRepository;
+import com.example.runningservice.repository.CrewRepository;
+import com.example.runningservice.repository.MemberRepository;
+import com.example.runningservice.util.S3FileUtil;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@ExtendWith(MockitoExtension.class)
+class CrewServiceTest {
+
+    @Mock
+    private CrewRepository crewRepository;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private CrewMemberRepository crewMemberRepository;
+    @Mock
+    private S3FileUtil s3FileUtil;
+    @InjectMocks
+    private CrewService crewService;
+
+    @Test
+    @DisplayName("크루 생성 - 이미지 O")
+    void createCrew_WithImage() {
+        // given
+        Long leaderId = 1L;
+        Create create = Create.builder()
+            .leaderId((leaderId))
+            .crewImage(new MockMultipartFile("file", new byte[]{1, 2, 3}))
+            .build();
+
+        MemberEntity memberEntity = MemberEntity.builder().id(leaderId).build();
+        CrewEntity crewEntity = CrewEntity.toEntity(create, memberEntity);
+
+        given(memberRepository.findById(leaderId)).willReturn(Optional.of(memberEntity));
+        given(crewRepository.save(any(CrewEntity.class))).willReturn(crewEntity);
+        given(s3FileUtil.getImgUrl(anyString())).willReturn("http://example.com/image");
+        doNothing().when(s3FileUtil).putObject(anyString(), any(MultipartFile.class));
+
+        // when
+        CrewData response = crewService.createCrew(create);
+
+        // then
+        assertEquals(crewEntity.getCrewId(), response.getCrewId());
+        verify(s3FileUtil, times(1)).putObject("crew-" + crewEntity.getCrewId(),
+            create.getCrewImage());
+        verify(s3FileUtil, times(1)).getImgUrl("crew-" + crewEntity.getCrewId());
+        verify(crewRepository, times(1)).save(any(CrewEntity.class));
+    }
+
+    @Test
+    @DisplayName("크루 생성 - 이미지 X")
+    public void createCrew_WithoutImage() {
+        // given
+        Long leaderId = 1L;
+        Create newCrew = Create.builder()
+            .leaderId(leaderId)
+            .build();
+
+        MemberEntity memberEntity = new MemberEntity();
+        CrewEntity crewEntity = CrewEntity.toEntity(newCrew, memberEntity);
+
+        given(memberRepository.findById(leaderId)).willReturn(Optional.of(memberEntity));
+        given(crewRepository.save(any(CrewEntity.class))).willReturn(crewEntity);
+        given(s3FileUtil.getImgUrl(anyString())).willReturn("http://example.com/default");
+
+        // when
+        CrewData response = crewService.createCrew(newCrew);
+
+        // then
+        assertEquals(crewEntity.getCrewId(), response.getCrewId());
+        // 이미지가 없는 경우 putObject는 호출되지 않아야 함
+        verify(s3FileUtil, never()).putObject(anyString(), any(MultipartFile.class));
+        verify(s3FileUtil, times(1)).getImgUrl("crew-default");
+        verify(crewRepository, times(1)).save(any(CrewEntity.class));
+    }
+
+    @Test
+    @DisplayName("크루 생성 (실패) - 사용자 없음")
+    public void createCrew_UserNotFound() {
+        // given
+        Long leaderId = 1L;
+        Create newCrew = Create.builder()
+            .leaderId(leaderId)
+            .build();
+
+        given(memberRepository.findById(leaderId)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(CustomException.class, () -> crewService.createCrew(newCrew));
+    }
+}
