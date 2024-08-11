f2c767c0f260e600e34101018ca09c5f5e533db0
yeon <p-yo00@naver.com>
2024-08-12 04:37:23 +0900

    Revert "feat : 크루명 중복 Validator 생성"
    This reverts commit 54158ac1552e9e0eb5b4701666eef86bc84a3bfd.

--- src/main/java/com/example/runningservice/repository/CrewRepository.java
+++ src/main/java/com/example/runningservice/repository/CrewRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CrewRepository extends JpaRepository<CrewEntity, Long> {
 
-    boolean existsByCrewName(String crewName);
 }
