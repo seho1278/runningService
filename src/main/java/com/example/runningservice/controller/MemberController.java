d12cfce1346c31719f73ecea716013088cdaaef5
Sim Juhyun <154858191+JuhyunSim@users.noreply.github.com>
2024-08-12 14:08:47 +0900

    feat : Signup-001 회원정보 저장 로직 구현
    회원가입 비즈니스 로직 작성(controller, service)
    회원가입 관련 Entity(memberEntity)
    예외처리 코드
    
    테스트코드 작성 미진행
    Http 테스트만 진행 / 회원정보 저장 내용 확인

--- src/main/java/com/example/runningservice/controller/MemberController.java
+++ src/main/java/com/example/runningservice/controller/MemberController.java
@@ -7,7 +7,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,18 +26,27 @@ public class MemberController {
     public ResponseEntity<MemberResponseDto> signup(
         @RequestBody @Valid SignupRequestDto signupRequestDto) throws Exception {
         // 프로필 이미지 처리
-        if (signupRequestDto.getProfileImage() != null && !signupRequestDto.getProfileImage()
-            .isEmpty()) {
-//            String imageUrl = userService.saveProfileImage(signupRequestDto.getProfileImage());
-            // 사용자 정보 저장 시 이미지 URL도 저장
-        } else {
-//            String defaultImageUrl = userService.getDefaultProfileImageUrl();
-            // 사용자 정보 저장 시 기본 이미지 URL 저장
-        }
+//        if (signupRequestDto.getProfileImage() != null && !signupRequestDto.getProfileImage()
+//            .isEmpty()) {
+////            String imageUrl = userService.saveProfileImage(signupRequestDto.getProfileImage());
+//            // 사용자 정보 저장 시 이미지 URL도 저장
+//        } else {
+////            String defaultImageUrl = userService.getDefaultProfileImageUrl();
+//            // 사용자 정보 저장 시 기본 이미지 URL 저장
+//        }
 
         // 사용자 정보 저장 로직
-        return ResponseEntity.ok(memberService.registerUser(signupRequestDto));
+        return ResponseEntity.ok(memberService.signup(signupRequestDto));
     }
 
+    @PostMapping("/signup/email-send")
+    ResponseEntity<?> sendVerifyEmail(@RequestParam String email) {
+        memberService.sendEmail(email);
+        return ResponseEntity.ok().build();
+    }
 
+    @PutMapping("/signup/email-verify")
+    ResponseEntity<Boolean> verifyUser(@RequestParam String email, @RequestParam String code) {
+        return ResponseEntity.ok(memberService.verifyUser(email, code));
+    }
 }
