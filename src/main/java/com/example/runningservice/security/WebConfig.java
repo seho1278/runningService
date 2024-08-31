d9167d061132ad9de8de13472bd7091859b22d26
Sim Juhyun <154858191+JuhyunSim@users.noreply.github.com>
2024-08-31 20:41:57 +0900

    fix - CORS-Use Cors 활성화 설정2
    - Cors 사용할 수 있도록 설정
      - Spring Security 설정 : cors 허용, 빈 등록
      - spring webMvc 설정 : 삭제

--- src/main/java/com/example/runningservice/security/WebConfig.java
+++ src/main/java/com/example/runningservice/security/WebConfig.java
@@ -3,7 +3,6 @@ package com.example.runningservice.security;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,14 +11,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserResolver());
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOriginPatterns("http://localhost:*") // 모든 도메인 허용
-            .allowedMethods("*") // 모든 HTTP 메서드 허용
-            .allowedHeaders("*") // 모든 헤더 허용
-            .allowCredentials(true); // 쿠키 전송 허용
     }
 }
