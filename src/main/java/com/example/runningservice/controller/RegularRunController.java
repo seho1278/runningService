e05897d21a4c5e5f03e9c50554d71f33236d9161
yeon <p-yo00@naver.com>
2024-09-01 11:22:20 +0900

    feat : 정기러닝 페이징 DTO 생성
    변경 사유:
    - 정기러닝의 구조상 DTO를 따로 두어 페이징 값을 처리

--- src/main/java/com/example/runningservice/controller/RegularRunController.java
+++ src/main/java/com/example/runningservice/controller/RegularRunController.java
@@ -6,9 +6,7 @@ import com.example.runningservice.dto.regular_run.RegularRunRequestDto;
 import com.example.runningservice.dto.regular_run.RegularRunResponseDto;
 import com.example.runningservice.service.RegularRunService;
 import com.example.runningservice.util.LoginUser;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -68,7 +66,7 @@ public class RegularRunController {
      * 크루별 정기러닝 정보 조회
      */
     @GetMapping("/crew/regular")
-    public ResponseEntity<Page<CrewRegularRunResponseDto>> getRegularRunList(Pageable pageable) {
+    public ResponseEntity<CrewRegularRunResponseDto> getRegularRunList(Pageable pageable) {
         return ResponseEntity.ok(regularRunService.getRegularRunList(pageable));
     }
 
@@ -76,7 +74,7 @@ public class RegularRunController {
      * 특정 크루 정기 러닝 정보 조회
      */
     @GetMapping("/crew/{crewId}/regular")
-    public ResponseEntity<Map<String, Object>> getCrewRegularRunList(
+    public ResponseEntity<CrewRegularRunResponseDto> getCrewRegularRunList(
         @PathVariable("crewId") Long crewId, Pageable pageable) {
         return ResponseEntity.ok(regularRunService.getCrewRegularRunList(crewId, pageable));
     }
--- src/main/java/com/example/runningservice/service/RegularRunService.java
+++ src/main/java/com/example/runningservice/service/RegularRunService.java
@@ -1,6 +1,7 @@
 package com.example.runningservice.service;
 
 import com.example.runningservice.dto.regular_run.CrewRegularRunResponseDto;
+import com.example.runningservice.dto.regular_run.CrewRegularRunResponseDto.Content;
 import com.example.runningservice.dto.regular_run.RegularRunRequestDto;
 import com.example.runningservice.dto.regular_run.RegularRunResponseDto;
 import com.example.runningservice.entity.CrewEntity;
@@ -9,13 +10,11 @@ import com.example.runningservice.exception.CustomException;
 import com.example.runningservice.exception.ErrorCode;
 import com.example.runningservice.repository.RegularRunMeetingRepository;
 import com.example.runningservice.repository.crew.CrewRepository;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -81,7 +80,7 @@ public class RegularRunService {
      * 크루별 정기러닝 정보 조회
      */
     @Transactional
-    public Page<CrewRegularRunResponseDto> getRegularRunList(Pageable pageable) {
+    public CrewRegularRunResponseDto getRegularRunList(Pageable pageable) {
         // 갯수만큼 크루 ID를 조회하고, 크루 ID에 해당하는 모든 정기러닝 조회
         Page<CrewEntity> crewEntities = crewRepository.findAll(pageable);
 
@@ -99,33 +98,38 @@ public class RegularRunService {
                 )
             ));
 
-        return new PageImpl<>(crewRegularMap.entrySet().stream()
-            .map(entry -> CrewRegularRunResponseDto.builder()
+        List<Content> content = crewRegularMap.entrySet().stream()
+            .map(entry -> CrewRegularRunResponseDto.Content.builder()
                 .crewId(entry.getKey())
                 .data(entry.getValue())
                 .build())
-            .collect(Collectors.toList()),
-            crewEntities.getPageable(),
-            crewEntities.getSize());
+            .toList();
+
+        return CrewRegularRunResponseDto.builder()
+            .content(content)
+            .totalElements(crewEntities.getTotalElements())
+            .totalPages(crewEntities.getTotalPages())
+            .build();
     }
 
     /**
      * 특정 크루의 정기러닝 정보 조회
      */
-    public Map<String, Object> getCrewRegularRunList(Long crewId, Pageable pageable) {
+    public CrewRegularRunResponseDto getCrewRegularRunList(Long crewId, Pageable pageable) {
         Page<RegularRunMeetingEntity> crewEntities = regularRunMeetingRepository.findByCrew_Id(
             crewId, pageable);
 
-        // 구조가 바뀌므로 페이징 값은 따로 넣어줌
-        Map<String, Object> response = new HashMap<>();
-        response.put("totalElements", crewEntities.getTotalElements());
-        response.put("totalPages", crewEntities.getTotalPages());
-        response.put("content", CrewRegularRunResponseDto.builder()
+        Content content = CrewRegularRunResponseDto.Content.builder()
             .crewId(crewId)
-            .data(crewEntities.getContent().stream().map(RegularRunResponseDto::fromEntity).toList())
-            .build());
+            .data(crewEntities
+                .map(RegularRunResponseDto::fromEntity)
+                .toList()).build();
 
-        return response;
+        return CrewRegularRunResponseDto.builder()
+            .totalPages(crewEntities.getTotalPages())
+            .totalElements(crewEntities.getTotalElements())
+            .content(content)
+            .build();
     }
 
     /**
